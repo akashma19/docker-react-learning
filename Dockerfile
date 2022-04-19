1d1d50e1ed57e7d3a7c17ad54ef8b658ad46903e
akashma19 <ashu.ma19@gmail.com>
2022-04-19 19:26:21 -0400

    changed app text

--- Dockerfile
+++ Dockerfile
@@ -13,5 +13,7 @@ RUN npm run build
 
 FROM nginx
 
+
+
 COPY  --from=builder /app/build /usr/share/nginx/html
 
--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hi, there !!
-         I was changed
+         I was changed on the feature branch
          
         </p>
         <p>Working perfect</p>
